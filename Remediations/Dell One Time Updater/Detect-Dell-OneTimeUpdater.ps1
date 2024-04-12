dd08bf9acfc96aa5e5783ae52d83f0bd0bc8f1d8
Mads Johansen <mads@evilevil.dk>
2024-04-12 18:13:23 +0200

    Delete Remediations/Dell One Time Updater directory

--- Remediations/Dell One Time Updater/Detect-Dell-OneTimeUpdater.ps1
+++
@@ -1,3 +0,0 @@
-if (!(($(gwmi win32_bios).Manufacturer -like '*Dell*'))) {Write-output "This is not a dell Device. unable to continue" ; exit}
-Write-output "Starting one-time update of Dell device using Dell Command | Update"
-exit 1
