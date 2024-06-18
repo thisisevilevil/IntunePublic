711fee76683968c323cfd298c9213191f00aab98
Mads Johansen <mads@evilevil.dk>
2024-06-18 18:36:32 +0200

    Update UpgradeToWindows11.ps1

--- Scripts/Win11Unsupported/UpgradeToWindows11.ps1
+++ Scripts/Win11Unsupported/UpgradeToWindows11.ps1
@@ -1,8 +1,11 @@
-$currentdate = Get-Date -format 'ddMMyyyy_HHmmss'
-$dir = "C:\Windows\Logs\Win11_Update_$currentdate"
-New-Item $dir -ItemType Directory
+$dir = "C:\Windows\temp"
 $webClient = New-Object System.Net.WebClient
 $url = 'https://go.microsoft.com/fwlink/?linkid=2171764'
 $file = "$($dir)\Windows11InstallationAssistant.exe"
-$webClient.DownloadFile($url,$file)
-Start-Process -FilePath $file -Wait -ArgumentList "/quietinstall /skipeula /auto upgrade /copylogs $dir"
+
+if (!(Test-Path $file)) {Write-host "Downloading Windows 11 Installation assistant from the interwebz"} 
+{
+$webClient.DownloadFile($url,$file) 
+}
+
+Start-Process -FilePath $file -Wait -ArgumentList "/quietinstall /skipeula /auto upgrade"
