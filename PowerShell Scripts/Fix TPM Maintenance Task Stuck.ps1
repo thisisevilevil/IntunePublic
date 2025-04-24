8306451ccdd822110606c4de1e833396b3cba56e
Mads Johansen <mads@evilevil.dk>
2025-04-24 21:15:17 +0200

    Fixed a typo

--- PowerShell Scripts/Fix TPM Maintenance Task Stuck.ps1
+++ PowerShell Scripts/Fix TPM Maintenance Task Stuck.ps1
@@ -2,5 +2,5 @@
 
 $path = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\OOBE'
 New-ItemProperty -Path $path -Name 'SetupDisplayedEula' -PropertyType 'DWORD' -Value '1' -Force -Verbose
-Start-ScheduledTask -TaskName 'Microsoft\Windows\Platform\TpmMaintenance' -Verbose
+Start-ScheduledTask -TaskName 'Microsoft\Windows\TPM\Tpm-Maintenance' -Verbose
 Start-Sleep -Seconds 10
