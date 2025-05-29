b2e9aa32c25a9f34dffab0079e7650e8e09f852f
Mads Johansen <mads@evilevil.dk>
2025-05-29 11:31:58 +0200

    Update Win32app-DetectionScript-Intune-DellTPM.ps1

--- Packages/Dell Enable TPM w. BIOS Password/Win32app-DetectionScript-Intune-DellTPM.ps1
+++ Packages/Dell Enable TPM w. BIOS Password/Win32app-DetectionScript-Intune-DellTPM.ps1
@@ -1,3 +1,8 @@
+#Dell Specific TPM Check
 $cctk = 'C:\Program Files\Dell\EndpointConfigure\X86_64\cctk.exe'
 $tpmcheck = cmd /C $cctk --Tpm
 if ($tpmcheck -eq 'TpmSecurity=Enabled') {Write-host "TPM is Enabled"}
+
+#Alternative TPM Check
+$tpmstatus = (Get-TPM).TpmPresent
+if ($tpmstatus) {Write-host "TPM is enabled"}
