bc926e4310553b57b25613d60e2158421b90959b
Mads Johansen <mads@evilevil.dk>
2025-11-12 09:11:10 +0100

    Update Deploy-SecureBoot-OptIn-Key.ps1

--- PowerShell Scripts/Deploy-SecureBoot-OptIn-Key.ps1
+++ PowerShell Scripts/Deploy-SecureBoot-OptIn-Key.ps1
@@ -1,6 +1,6 @@
 #Deploys a registry key to enable Microsoft Update managed Secure Boot key opt-in
 #reference: https://support.microsoft.com/en-us/topic/registry-key-updates-for-secure-boot-windows-devices-with-it-managed-updates-a7be69c9-4634-42e1-9ca1-df06f43f360d
-$Path  = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\Servicing'
+$Path  = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\'
 $Name  = 'MicrosoftUpdateManagedOptIn'
 $Value = '1'
 
