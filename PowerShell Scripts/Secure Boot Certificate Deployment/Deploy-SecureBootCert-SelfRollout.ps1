7b3903c0f5de188d702780da0ec5e2ad74483a0e
Mads Johansen <mads@evilevil.dk>
2025-11-22 17:53:59 +0100

    Update Deploy-SecureBootCert-SelfRollout.ps1

--- PowerShell Scripts/Secure Boot Certificate Deployment/Deploy-SecureBootCert-SelfRollout.ps1
+++ PowerShell Scripts/Secure Boot Certificate Deployment/Deploy-SecureBootCert-SelfRollout.ps1
@@ -1,5 +1,5 @@
 #Deploys a registry key to enable Microsoft Update managed Secure Boot key opt-in
-#reference: https://evil365.com/intune/SecureBoot-Cert-Expiration/#option-2---automatic-rollout-via-microsoft-controlled-feature-rollout-cfr
+#reference: https://evil365.com/intune/SecureBoot-Cert-Expiration/#option-3---self-managed-rollout-using-intune-policies
 #reference: https://support.microsoft.com/en-us/topic/registry-key-updates-for-secure-boot-windows-devices-with-it-managed-updates-a7be69c9-4634-42e1-9ca1-df06f43f360d#bkmk_registry_keys
 
 $Path  = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\'
