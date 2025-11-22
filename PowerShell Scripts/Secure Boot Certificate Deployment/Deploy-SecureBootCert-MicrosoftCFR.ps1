e67d4ccf6ce09650fc9cd994c44af329f61b4b1f
Mads Johansen <mads@evilevil.dk>
2025-11-22 12:28:00 +0100

    Updated Secure Boot Cert rollout keys

---
+++ PowerShell Scripts/Secure Boot Certificate Deployment/Deploy-SecureBootCert-MicrosoftCFR.ps1
@@ -0,0 +1,10 @@
+#Deploys a registry key to enable Microsoft Update managed Secure Boot key opt-in
+#reference: https://evil365.com/intune/SecureBoot-Cert-Expiration/#option-2---automatic-rollout-via-microsoft-controlled-feature-rollout-cfr
+#reference: https://support.microsoft.com/en-us/topic/registry-key-updates-for-secure-boot-windows-devices-with-it-managed-updates-a7be69c9-4634-42e1-9ca1-df06f43f360d#bkmk_registry_keys
+
+$Path  = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot\'
+$Name  = 'MicrosoftUpdateManagedOptIn'
+$Value = '1' 
+
+if (!(Test-Path $Path)) {New-Item -Path $Path -Force}
+New-ItemProperty -Path $Path -Name $Name -PropertyType DWord -Value $Value -Force
--- Remediations/Clear WindowsUpdate Policies/Remediate-RemediateWindowsUpdatePolicies.ps1
+++ Remediations/Clear WindowsUpdate Policies/Remediate-RemediateWindowsUpdatePolicies.ps1
@@ -1,1 +1,2 @@
 Remove-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\*WindowsUpdate*" -Recurse
+restart-service wuauserv
